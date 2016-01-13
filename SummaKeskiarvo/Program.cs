66ac05458da30926f12033822c9ffd4d84ee2ecd
Linkka <samilindroos@hotmail.com>
2016-01-13 13:26:35 +0200

    Initial commit.

---
+++ SummaKeskiarvo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Program.Main();
+
+namespace SummaKeskiarvo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kysytään käyttäjältä luku
+            Console.Write("Anna 3 lukua: ");
+            // int luku = int.Parse(rivi); // 1
+            
+            int luku1,luku2,luku3;
+            luku1 = int.Parse(Console.ReadLine());
+            luku2 = int.Parse(Console.ReadLine());
+            luku3 = int.Parse(Console.ReadLine());
+
+            int summa = (luku1 + luku2 + luku3);
+            int keskiarvo = (summa / 3);
+            Console.WriteLine("Lukujen summa: " + summa);
+            Console.WriteLine("Lukujen keskiarvo: " + keskiarvo);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SummaKeskiarvo/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SummaKeskiarvo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SummaKeskiarvo")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("135d155d-2257-44d4-89b8-5cb97b27f541")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
